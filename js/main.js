f3d87d405ee6bfbb306b8e2d1facf20f8d4c5caa
TECH-A58 <tech-a58@TECH-A58s-MacBook-Air.local>
2018-06-04 12:25:03 +0100

    Created a moving ball that moves across the whole screen, needs to be fixed to game screen

--- js/main.js
+++ js/main.js
@@ -1,5 +1,53 @@
-var ball
-var paddle
-var brick
-var lives = 3;
-var score = 0;
+$(document).ready(function() {
+
+  var $ball = $("#ball");
+  var paddle;
+  var firebrick;
+  var $gameScreen = $("#gameScreen");
+  var lives = 3;
+  var score = 0;
+
+  var interval;
+  var timeRunning = false;
+
+  var posX = 0;
+  var posY = 0;
+
+  var directionX = "+";
+  var directionY = "+";
+
+  $("#btn").click(function(){
+
+    if (timeRunning == "true") {
+      clearInterval(interval)
+      timeRunning = !timeRunning;
+    } else {
+      interval = setInterval(function () {
+        var ballLeft = $ball.offset.left;
+        var balltop = $ball.offset.top;
+
+        if (directionX === "+") {
+          $ball.css({'left': posX + "px"});
+          posX += 1;
+        }
+        else if (directionX === "-") {
+          $ball.css({"left" : posX + "px"});
+          posX -= 1;
+        }
+        if (directionY === "+") {
+          $ball.css({'top': posY + "px"});
+          posY += 1;
+        }
+        else if (directionY === "-") {
+          $ball.css({"top" : posY + "px"});
+          posY -= 1;
+        }
+
+        timeRunning = !timeRunning;
+
+      }, 10);
+    }
+
+  })
+
+})
