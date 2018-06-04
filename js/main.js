23311aed4bab68bd76dc6976c002d875795acb4f
TECH-A58 <tech-a58@TECH-A58s-MacBook-Air.local>
2018-06-04 15:31:42 +0100

    Created the physics behind the boucing ball

--- js/main.js
+++ js/main.js
@@ -16,18 +16,32 @@ $(document).ready(function() {
   var directionX = "+";
   var directionY = "+";
 
+
+  // var offset = $ball.offset;
+
   $("#btn").click(function(){
 
     if (timeRunning == "true") {
+      // reset the ball to original position and set timeRunning to false
       clearInterval(interval)
       timeRunning = !timeRunning;
     } else {
       interval = setInterval(function () {
-        var ballLeft = $ball.offset.left;
-        var balltop = $ball.offset.top;
+        var ballLeft = $ball.offset().left;
+        var ballTop = $ball.offset().top;
+        var ballRight = ballLeft + $ball.outerWidth();
+        var ballBottom = ballTop + $ball.outerHeight();
 
+        var gsLeft = $gameScreen.offset().left;
+        var gsTop = $gameScreen.offset().top;
+        var gsRight = gsLeft + $gameScreen.outerWidth();
+        var gsBottom = gsTop + $gameScreen.outerHeight();
+
+        // sets absolute starting point for the ball
+        // sets direction of movement
         if (directionX === "+") {
           $ball.css({'left': posX + "px"});
+          // sets the number of pixels the ball moves per set time interval
           posX += 1;
         }
         else if (directionX === "-") {
@@ -43,11 +57,21 @@ $(document).ready(function() {
           posY -= 1;
         }
 
-        timeRunning = !timeRunning;
+        if (ballBottom >= gsBottom) {
+          directionY = '-';
+        }  else if (ballRight >= gsRight) {
+          directionX = "-";
+        } else if (ballLeft <= gsLeft) {
+          directionX = "+";
+        } else if (ballTop <= gsTop) {
+          directionY = "+";
+        }
 
       }, 10);
+      timeRunning = !timeRunning;
     }
 
   })
+
 
 })
